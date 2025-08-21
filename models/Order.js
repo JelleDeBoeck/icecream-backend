140f54925b4320fba996206fc22952fc23a0960e
Jelle De Boeck <deboeck07011@gmail.com>
2025-08-21 09:57:54 +0200

    feat: vereisten bestelling geupdate

--- models/Order.js
+++ models/Order.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
   customerName: { type: String, required: true },
   address: { type: String, required: true },
-  flavor: { type: String, required: true },
-  topping: { type: String, required: true },
-  status: { type: String, default: 'te verwerken' },
   totalPrice: { type: Number, required: true },
+  // Optioneel: je kan hier later smaken/toppings toevoegen als je wilt
 }, { timestamps: true });
 
 module.exports = mongoose.model('Order', orderSchema);
--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Order = require('../models/Order');
 
-// TEST route om te checken of router werkt
+// TEST route
 router.get('/test', (req, res) => {
   res.send('Router werkt!');
 });
